73fd252eec14909fa18f43e850a94d86a7e6dbe7
Stephen Oberauer <losthobbit@gmail.com>
2015-01-04 19:41:18 +0000

    Added hierarchy of tags view

---
+++ Transparent/Routines/JavaScript.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Transparent.Routines
+{
+    public static class JavaScript
+    {
+        /// <summary>
+        /// Serialize a POCO class for use as JSON in a view.
+        /// </summary>
+        public static IHtmlString SerializeObject(object value)
+        {
+            using (var stringWriter = new StringWriter())
+            using (var jsonWriter = new JsonTextWriter(stringWriter))
+            {
+                var serializer = new JsonSerializer
+                {
+                    // Let's use camelCasing as is common practice in JavaScript
+                    ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                };
+
+                // We don't want quotes around object names
+                jsonWriter.QuoteName = false;
+                serializer.Serialize(jsonWriter, value);
+
+                return new HtmlString(stringWriter.ToString());
+            }
+        }
+    }
+}
