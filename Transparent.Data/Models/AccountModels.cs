118485ab688f166186030782f9dc91899a1ba688
Stephen Oberauer <losthobbit@gmail.com>
2015-04-30 21:38:28 +0100

    Increased min password length to 8 chars.

--- Transparent.Data/Models/AccountModels.cs
+++ Transparent.Data/Models/AccountModels.cs
@@ -65,7 +65,7 @@ namespace Transparent.Data.Models
         public string OldPassword { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
         [DataType(DataType.Password)]
         [Display(Name = "New password")]
         public string NewPassword { get; set; }
@@ -121,7 +121,7 @@ namespace Transparent.Data.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
